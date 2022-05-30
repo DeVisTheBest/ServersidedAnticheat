e3be8e9d49a00188d847eb71fa5b61a05a18bbd8
DeV <95656165+DeVisTheBest@users.noreply.github.com>
2022-05-29 22:18:41 -0300

    Update AntiInfJump.lua

--- AntiInfJump.lua
+++ AntiInfJump.lua
@@ -13,7 +13,7 @@ game.Players.PlayerAdded:Connect(function(player)
 			if not raycast then -- Dont want them to die if they just fell from a building right?
 				print(root.Position.Y, lastMagnitude[player.Name])
 				if (root.Position.Y > lastMagnitude[player.Name]) then
-					if not player.Health == 0 then
+					if not humanoid.Health == 0 then
 						-- When testing the client version, i figured out that because of the little fling you get when you get killed it thinks you're using a cheat
 						print("Punishing player ".. player.Name .. " | Reason: Infinite Jump")
 						humanoid:TakeDamage(humanoid.Health) -- Do whatever you want here, i would kill the player but you can kick them if you want
